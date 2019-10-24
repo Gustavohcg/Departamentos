5576147718af34808329784fb0f994723dc72434
Gustavo Guerreiro <gustavo.carvalhal.guerreiro@gmail.com>
2019-10-24 16:07:29 -0300

    VC

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Departamentos.Models;
+using Departamentos.Models.ViewModels;
 
 namespace Departamentos.Controllers {
     public class HomeController : Controller {
--- Models/ViewModels/ErrorViewModel.cs
+++ Models/ViewModels/ErrorViewModel.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace Departamentos.Models {
+namespace Departamentos.Models.ViewModels {
     public class ErrorViewModel {
         public string RequestId { get; set; }
 
