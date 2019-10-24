90a83b4e962bacfd2cdef7658d90478c6de13e9b
Gustavo Guerreiro <gustavo.carvalhal.guerreiro@gmail.com>
2019-10-24 16:22:55 -0300

    Scafould

---
+++ Data/DepartamentosContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Departamentos.Models
+{
+    public class DepartamentosContext : DbContext
+    {
+        public DepartamentosContext (DbContextOptions<DepartamentosContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Departamentos.Models.Departments> Departments { get; set; }
+    }
+}
